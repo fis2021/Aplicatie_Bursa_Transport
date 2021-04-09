b5dd88c025ece205cb66040d5670aa402cee0f3d
Augustin <catalin.rotar@student.upt.ro>
2021-04-09 14:10:00 +0300

    fistpage

---
+++ src/main/java/org/app/transport/controllers/FirstPageController.java
@@ -0,0 +1,23 @@
+package org.app.transport.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+
+public class FirstPageController {
+
+
+    @FXML
+    Button button1;
+
+    public void handle1(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+
+    }
+}
+
