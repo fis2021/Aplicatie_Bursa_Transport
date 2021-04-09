b2026c1922656a5d08db695905be201b457c0673
Augustin <catalin.rotar@student.upt.ro>
2021-04-09 15:05:56 +0300

    PT-17 Create a file to persist user credentials

--- src/main/java/org/app/transport/controllers/RegisterController.java
+++ src/main/java/org/app/transport/controllers/RegisterController.java
@@ -1,9 +1,20 @@
 package org.app.transport.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import org.app.transport.exceptions.UsernameAlreadyExistsException;
+import org.app.transport.services.UserService;
 
 public class RegisterController {
     public void handleRegisterAction(ActionEvent actionEvent) {
 
+    }
+
+    public void handle2(MouseEvent mouseEvent) {
     }
 }
