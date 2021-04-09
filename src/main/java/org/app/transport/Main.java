b8adf6bbc5fcd67131926149c0c85e7030642512
Augustin <catalin.rotar@student.upt.ro>
2021-04-09 14:10:00 +0300

    fistpage

---
+++ src/main/java/org/app/transport/Main.java
@@ -0,0 +1,24 @@
+package org.app.transport;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/firstpage.fxml"));
+        Scene scene = new Scene(root, 500, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
