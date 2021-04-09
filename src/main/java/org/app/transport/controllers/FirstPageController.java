f0b010cc4c298e85fc76c7d00ccc75cdea5c1d16
Augustin <catalin.rotar@student.upt.ro>
2021-04-09 14:22:10 +0300

    PT-18 Create a gui login form

--- src/main/java/org/app/transport/controllers/FirstPageController.java
+++ src/main/java/org/app/transport/controllers/FirstPageController.java
@@ -17,7 +17,9 @@ public class FirstPageController {
     Button button1;
 
     public void handle1(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
-
+        Parent root = FXMLLoader.load(getClass().getResource("/register.fxml"));
+        Stage  window = (Stage)button1.getScene().getWindow();
+        window.setScene(new Scene(root, 750,500));
     }
 }
 
---
+++ src/main/java/org/app/transport/controllers/RegisterController.java
@@ -0,0 +1,9 @@
+package org.app.transport.controllers;
+
+import javafx.event.ActionEvent;
+
+public class RegisterController {
+    public void handleRegisterAction(ActionEvent actionEvent) {
+
+    }
+}
