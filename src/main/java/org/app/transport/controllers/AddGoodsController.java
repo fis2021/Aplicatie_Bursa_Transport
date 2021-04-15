8053713a6f361122f0331c1af095ec97954c7ce8
Augustin <catalin.rotar@student.upt.ro>
2021-04-15 16:44:20 +0300

    PT-21 #comment Create a popup window to add good objects (Add goods)

--- src/main/java/org/app/transport/controllers/AddGoodsController.java
+++ src/main/java/org/app/transport/controllers/AddGoodsController.java
@@ -2,8 +2,7 @@ package org.app.transport.controllers;
 
 import javafx.scene.input.MouseEvent;
 
-public class MyGoodsList {
+public class AddGoodsController {
     public void handleReturn(MouseEvent mouseEvent) {
-
     }
 }
