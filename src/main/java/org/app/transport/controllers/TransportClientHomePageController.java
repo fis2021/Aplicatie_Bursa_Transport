7a6b111a25b99ef3d5236e0976e748013e0b7009
Augustin <catalin.rotar@student.upt.ro>
2021-04-15 16:08:19 +0300

    PT-20 #comment Create a GUI with a list view and link it to Registration page (My goods details page)

--- src/main/java/org/app/transport/controllers/TransportClientHomePageController.java
+++ src/main/java/org/app/transport/controllers/TransportClientHomePageController.java
@@ -1,6 +1,9 @@
 package org.app.transport.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -11,8 +14,17 @@ import java.io.IOException;
 public class TransportClientHomePageController {
     @FXML
      Button closeButton;
+    @FXML
+    private Button DisplayGoodsList;
     public void handle4(MouseEvent mouseEvent) {
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
+    }
+
+    public void handleGoodsList(MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/MyGoodsList.fxml"));
+        Stage  window = (Stage)DisplayGoodsList.getScene().getWindow();
+        window.setScene(new Scene(root, 500,400));
+
     }
 }
