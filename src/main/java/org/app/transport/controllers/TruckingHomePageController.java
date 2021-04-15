c876ffe1f77ef7653d70e4149a11585e522dd293
Augustin <catalin.rotar@student.upt.ro>
2021-04-15 10:51:22 +0300

    Log in style finish

--- src/main/java/org/app/transport/controllers/TruckingHomePageController.java
+++ src/main/java/org/app/transport/controllers/TruckingHomePageController.java
@@ -1,8 +1,18 @@
 package org.app.transport.controllers;
 
+import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class TruckingHomePageController {
+    @FXML
+    private Button closeButton;
     public void GetColor(MouseEvent mouseEvent) {
+    }
+
+    public void handlecloseButton(MouseEvent mouseEvent) {
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        stage.close();
     }
 }
