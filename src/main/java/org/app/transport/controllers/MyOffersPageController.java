6a92769f57fedf2721f12f950965afc51f55e327
Victor Trohin <trohin2000@gmail.com>
2021-05-05 16:10:27 +0300

    MyOffersPage added: price, due date, detailed addres and addtional information

--- src/main/java/org/app/transport/controllers/MyOffersPageController.java
+++ src/main/java/org/app/transport/controllers/MyOffersPageController.java
@@ -36,17 +36,34 @@ public class MyOffersPageController {
                 String[] split2 = s.split("\\|");
                 String[] split3 = split2[0].split("~");
                 String[] split4 = split2[1].split("-");
+
                 TreeItem<String> branchItem = new TreeItem<>("The Offer was sent to user:" + split3[0]);
+
                 TreeItem<String> leafItem0 = new TreeItem<>("Accept/Reject status");
                 TreeItem<String> leafItem = new TreeItem<>("Information about good");
                 TreeItem<String> leafItem1 = new TreeItem<>("Price and due date");
                 TreeItem<String> leafItem2 = new TreeItem<>("Information about delivery");
+
                 TreeItem<String> leaf0 = new TreeItem<>(split4[3] + ":" + split4[1] + "-" + split4[2] + "(" + split4[0] + ")");
-                leafItem.getChildren().add(leaf0);
-                TreeItem<String> leaf2 = new TreeItem<>("Only if the offer was accepted!");
-                leafItem2.getChildren().add(leaf2);
+                //leaf to show the address
+                TreeItem<String> leaf2 = new TreeItem<>("Detailed address: Visible if the offer was accepted!");
+                //leaf to show the additional information given
+                TreeItem<String> leaf21 = new TreeItem<>("Additional Information: Visible if the offer was accepted!");
+                //leaf to show the status
                 TreeItem<String> leaf3 = new TreeItem<>(split3[5]+"!");
-               leafItem0.getChildren().add(leaf3);
+                //leaf to show the price and due date
+                TreeItem<String> leaf1 = new TreeItem<>("Price: " + split3[2] + "; Due date: " + split3[3]);
+
+                if(split3[5].compareTo("Accepted") == 0){
+                    leaf21.setValue("Detailed address: " + split4[4]);
+                    leaf2.setValue("Information given by " + split3[0] + ": " + split3[4]);
+
+                }
+
+                leafItem.getChildren().add(leaf0);
+                leafItem1.getChildren().add(leaf1);
+                leafItem2.getChildren().addAll(leaf21,leaf2);
+                leafItem0.getChildren().add(leaf3);
                 branchItem.getChildren().addAll(leafItem0, leafItem, leafItem1, leafItem2);
                 rootItem.getChildren().add(branchItem);
             }
