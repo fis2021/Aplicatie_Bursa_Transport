862d8fce5355507dab1340f2eb5050619f1db72c
Augustin <catalin.rotar@student.upt.ro>
2021-05-15 16:59:02 +0300

    UserService innitDataBase test

---
+++ src/test/java/org/app/transport/services/UserServiceTest.java
@@ -0,0 +1,34 @@
+package org.app.transport.services;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+    public static final String ADMIN = "admin";
+
+    @BeforeEach
+    void setUp() throws IOException {
+        FileSystemService.APPLICATION_FOLDER = ".test-registration-example";
+        FileUtils.cleanDirectory(FileSystemService.getApplicationHomeFolder().toFile());
+        UserService.initDatabase();
+    }
+    @AfterEach
+    void tearDown() {
+        UserService.database.close();
+    }
+    @Test
+    @DisplayName("Database is initialized, and there are no users")
+    void testDatabaseIsInitializedAndNoUserIsPersisted() {
+        assertThat(UserService.getAllUsers()).isNotNull();
+        assertThat(UserService.getAllUsers()).isEmpty();
+    }
+
+}
