c4c48f04e81e8e9f56edd8b1b3223deef28859e0
Victor Trohin <trohin2000@gmail.com>
2021-05-15 12:36:28 +0300

    my Transactions ListView can't get the offer satus

--- src/main/java/org/app/transport/controllers/myTransactionsPageTransportClientController.java
+++ src/main/java/org/app/transport/controllers/myTransactionsPageTransportClientController.java
@@ -7,7 +7,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.app.transport.model.User;
 import org.app.transport.services.UserService;
@@ -18,13 +21,39 @@ import java.util.Arrays;
 public class myTransactionsPageTransportClientController {
     @FXML
     private Button ReturnHome;
+    @FXML
+    private ListView<String> listView;
+    @FXML
+    private Text message;
 
     private String username;
+    private String listElement;
+    private int n = 0;
+    private String LocT;
+    private String LocF;
+    private String listItem;
 
     public void setUsername(String username) {
         this.username = username;
         User c = UserService.FindTheUser(username);
-
+        if(c.getGood().isEmpty()||c.getGood().compareTo("*")==0)
+            message.setText("No element in the list");
+        else {
+            String[] splits = c.getGood().split("/");
+            for (String s : splits) {
+                System.out.println(Arrays.toString(splits));
+                String[] split2 = s.split("\\|");
+                String[] split3 = split2[0].split("~");
+                if(split3[5].equals("Closed"))
+                    listView.getItems().add(s);
+            }
+            listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+                @Override
+                public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                    listItem = listView.getSelectionModel().getSelectedItem();
+                }
+            });
+        }
     }
 
     public void handleReturn(MouseEvent mouseEvent) {
