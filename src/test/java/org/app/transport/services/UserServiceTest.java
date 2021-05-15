b192ad67cf7cdca26b1f4d53033f207a4f45cea8
Augustin <catalin.rotar@student.upt.ro>
2021-05-15 17:12:17 +0300

    UserService user update test

--- src/test/java/org/app/transport/services/UserServiceTest.java
+++ src/test/java/org/app/transport/services/UserServiceTest.java
@@ -55,4 +55,15 @@ class UserServiceTest {
             UserService.addUser(ADMIN, ADMIN, ADMIN,ADMIN);
         });
     }
+    @Test
+    @DisplayName("Update user check")
+    void testUserUpdate() throws IncorrectUsername, UsernameAlreadyExistsException, IncorrectPassword, RoleException {
+        UserService.addUser(ADMIN, ADMIN, ADMIN,"*");
+        assertThat(UserService.getAllUsers()).isNotEmpty();
+        assertThat(UserService.getAllUsers()).size().isEqualTo(1);
+        User user = UserService.getAllUsers().get(0);
+        user.setSomething("admin");
+        UserService.updateUser(user,user.getUsername());
+        assertThat(user.getGood()).isEqualTo("admin");
+    }
 }
